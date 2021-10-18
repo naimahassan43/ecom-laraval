9dc5337e75fbf2a560b3e8943ae32aff26ba3bd7
Naima Hassan <naima.hassan434@gmail.com>
2021-10-18 20:58:14 +0100

    product model -relationship among 4tables

--- resources/views/backend/includes/nav.blade.php
+++ resources/views/backend/includes/nav.blade.php
@@ -85,13 +85,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{route('category.create')}}" class="nav-link">
+                <a href="{{route('product.create')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Create Product</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{route('category.manage')}}" class="nav-link">
+                <a href="{{route('product.manage')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Manage Product</p>
                 </a>
